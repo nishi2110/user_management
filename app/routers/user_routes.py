711a4307e6536b80d7a1161ecb63fc03b7607f3d
Ahmed Bin Nayeem <ahmedbinnayeem@Ahmeds-MacBook-Pro.local>
2024-12-12 14:05:48 -0500

    added exception logs in user_routes

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -33,6 +33,11 @@ from app.services.jwt_service import create_access_token
 from app.utils.link_generation import create_user_links, generate_pagination_links
 from app.dependencies import get_settings
 from app.services.email_service import EmailService
+import logging
+
+def log_exception(exc: HTTPException):
+    logging.error(f"Error {exc.status_code}: {exc.detail}")
+
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 settings = get_settings()
