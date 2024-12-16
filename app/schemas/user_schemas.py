28335cc379c4ca2da07221ae2fcec8f4fcad7489
trimplindevelopment <bb472@njit.edu>
2024-12-17 00:46:06 +0530

    Update user_schemas.py
    Added validation for user passwords in user creation

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -51,6 +51,21 @@ class UserCreate(UserBase):
             raise ValueError("Email username cannot contain 'admin'.")
         
         return value
+    
+    @validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        if not any(char.isupper() for char in value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not any(char.islower() for char in value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not any(char.isdigit() for char in value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Password must contain at least one special character.")
+        return value
+
 
 
 class UserUpdate(UserBase):
